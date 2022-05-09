072a9183497384652aec4082f31fe5be30482e88
Mohamed Rashed <mhmadrashd27@gmail.com>
2022-05-09 02:37:57 +0200

    images paths modification

--- Back-End/assets/helpers/Images.js
+++ Back-End/assets/helpers/Images.js
@@ -3,25 +3,37 @@ const multer = require("multer");
 /** DiskStorage for Saving Images */
 
 const Storage = multer.diskStorage({
-        destination:(req, file, cb)=>{
-            cb(null, './assets/images')
-        },
-        filename:(req, file, cb)=>{
-        cb(null,Date.now() + file.originalname)
-    },
+  destination: (req, file, cb) => {
+    switch (req.baseUrl) {
+      case "/user":
+        cb(null, "./assets/images/users");
+        break;
+      case "/author":
+        cb(null, "./assets/images/authors");
+        break;
+      case "/book":
+        cb(null, "./assets/images/books");
+        break;
+      default:
+        cb(null, "./assets/images");
+        break;
+    }
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + file.originalname);
+  },
 });
 
 // Image mimetype validation
 /*
 
 
-*/ 
+*/
 const Upload = multer({
-        storage:Storage,
-        limits: { 
-            fileSize:1024 * 1024 * 5,
-        }
-    })
-    
+  storage: Storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+});
 
-module.exports = Upload;    
+module.exports = Upload;
