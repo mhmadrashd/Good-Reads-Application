db708abb680ae8d45157b2c380d01d704f9db6d6
RofidaReda <57877648+ROFIDA106@users.noreply.github.com>
2022-05-10 14:51:38 +0200

    firstcommit

---
+++ Frontend/App.js
@@ -0,0 +1,17 @@
+// import logo from './logo.svg';
+import './App.css';
+import  Login from './components/Login/Login';
+// import   from './components/LoginForm';
+
+ import React,{Component} from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
