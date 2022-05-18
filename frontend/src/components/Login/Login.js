13269490c431e91760b25dab34964c5eb36318b2
RofidaReda <57877648+ROFIDA106@users.noreply.github.com>
2022-05-18 21:03:50 +0200

    Design

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -4,12 +4,9 @@ import LoginForm from "./LoginForm";
 import AuthService from "../../services/AuthService";
 import { AuthContext } from "../../store/context";
 import { useNavigate } from "react-router-dom";
-// // import Stack from '@mui/material/Stack';
-// // import Button from "@mui/material/Button";
-// import Button, { ButtonProps } from "@mui/material/Button";
-// import { purple } from "@mui/material/colors";
-// import { styled } from "@mui/material/styles";
-// // import Stack from "@mui/material/Stack";
+// import "~mdb-ui-kit/css/mdb.min.css";
+// import * as mdb from "mdb-ui-kit"; // lib
+// import { Input } from "mdb-ui-kit"; // module
 
 import "./Login.css";
 
@@ -44,54 +41,71 @@ const Login = (props) => {
 
   useEffect(() => {
     resetForm();
-  }, []);
-
-  // const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
-  //   color: theme.palette.getContrastText(purple[500]),
-  //   backgroundColor: purple[500],
-  //   '&:hover': {
-  //     backgroundColor: purple[700],
-  //   },
-  // }));
-
+  });
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-5">
-            <input
-              type="text"
-              name="username"
-              value={values.username}
-              onChange={handleChange}
-              className="login-input"
-              placeholder="username"
-            />
+    <section className="text-center gradient">
+      {/* <!-- BackgroundImage --> */}
+      <div className="p-5 bg-image Background"></div>
 
-            <br />
-          </div>
-          <div className="col-5">
-            <input
-              type="password"
-              name="password"
-              value={values.password}
-              onChange={handleChange}
-              className="login-input"
-              placeholder="password"
-            />
+      <div className="card mx-4 mx-md-5 shadow-5-strong Shadow"></div>
 
-            <br />
-            <span style={{ color: "#555555" }}>
-              <input type="checkbox" />
-              Remember Me
-            </span>
-            <a href="/" style={{ color: "#555555" }}>
-              Forget it?
-            </a>
+      <div className="card-body   ">
+        <div className="row d-flex justify-content-center login">
+          <div className="col-lg-8">
+            {/* <!-- 2 column grid layout with text inputs for the first and last names --> */}
+
+            <div className="container">
+              <h2 className="fw-bold mb-5 fwForm">Login</h2>
+
+              <form onSubmit={handleSubmit}>
+                <div className="row card  formEdit">
+                  <div className="col mb-4">
+                    <div className="form-outline btnlogin mb-3 button">
+                      <input
+                        type="text"
+                        name="username"
+                        value={values.username}
+                        onChange={handleChange}
+                        className="button"
+                        placeholder="username"
+                      />
+                      <br />
+                    </div>
+
+                    <div className="col-5 btnlogin ">
+                      <input
+                        type="password"
+                        name="password"
+                        value={values.password}
+                        onChange={handleChange}
+                        className="button"
+                        placeholder="password"
+                      />
+                    </div>
+                    <br />
+                    {/* <!-- Checkbox --> */}
+                    <div className="form-check d-flex justify-content-center mb-4 btnlogin button">
+                      <input
+                        className="form-check-input me-2"
+                        type="checkbox"
+                        value=""
+                        id="form2Example33"
+                      />
+                      <label className="form-check-label btnlogin button">
+                        Remember Me
+                      </label>
+                    </div>
+                    <a href="/" style={{ color: "#555555" }}>
+                      Forget it?
+                    </a>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 };
 
